7f84cc230a044ea2d36a7f31ae7a626f07f6a011
antonio <antoniochequer@gmail.com>
2021-08-11 13:23:46 -0300

    fixed bugs

--- main.py
+++ main.py
@@ -17,6 +17,12 @@ screen.tracer(0)
 
 snake = Snake()
 
+screen.listen()
+screen.onkey(snake.up, 'Up')
+screen.onkey(snake.down, 'Down')
+screen.onkey(snake.left, 'Left')
+screen.onkey(snake.right, 'Right')
+
 
 gameison = True
 
--- snake.py
+++ snake.py
@@ -3,14 +3,23 @@
 from turtle import Turtle, Screen
 import random
 
+#constants
 STARTING_AXIS = [(0,0), (-20,0), (-40, 0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+RIGHT = 0
+LEFT = 180
+
+
 
 class Snake():
     def __init__(self):
         self.squares = []  
         self.create_snake()
-    
+        self.head = self.squares[0]
+
+
     def create_snake(self):
         for i in STARTING_AXIS:
             new_square = Turtle('square')
@@ -24,5 +33,22 @@ class Snake():
             new_x = self.squares[i - 1].xcor()
             new_y = self.squares[i - 1].ycor()
             self.squares[i].goto(new_x, new_y)
-        self.squares[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+    
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+    
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
 
