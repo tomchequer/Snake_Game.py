854946f56613d9a8bfee95ec77d9c9a3ae897914
antonio <antoniochequer@gmail.com>
2021-08-10 21:55:15 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+#It'll be a simple snake game.                                    #
+#Its one of my first games ever built in any programming language!#
+#I'm pretty hyped, so i wish that if you're testing this,         #
+#You be a little patient and complacent                           #
+#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+
