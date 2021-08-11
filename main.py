bfc2c211d2c74e08118e227f088c2fdd93d51d83
antonio <antoniochequer@gmail.com>
2021-08-10 22:36:49 -0300

    Made some progress with the snake.

--- main.py
+++ main.py
@@ -5,3 +5,41 @@
 #You be a little patient and complacent                           #
 #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
 
+from turtle import Screen, Turtle, isvisible
+import time
+
+screen = Screen()
+screen.setup(height=600, width=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+
+snake = []
+
+starting_axis = [(0,0), (-20,0), (-40, 0)]
+
+for i in starting_axis:
+    new_square = Turtle('square')
+    new_square.color('white')
+    new_square.penup()
+    new_square.goto(i)
+    snake.append(new_square)
+    
+
+gameison = True
+
+while gameison:
+    screen.update()
+    time.sleep(0.5)
+    for i in range(len(snake) - 1, 0, -1):
+        new_x = snake[i - 1].xcor()
+        new_y = snake[i - 1].ycor()
+        snake[i].goto(new_x, new_y)
+    snake[0].forward(20)
+        
+
+
+
+
+screen.exitonclick()
